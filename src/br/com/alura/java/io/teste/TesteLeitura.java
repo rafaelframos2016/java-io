74d34facd556c33ca29637a392b5677ee139e5d5
Rafael de Freitas <rafaelnegresco@hotmail.com>
2019-11-01 10:42:40 -0300

    terceiro commit

--- src/br/com/alura/java/io/teste/TesteLeitura.java
+++ src/br/com/alura/java/io/teste/TesteLeitura.java
@@ -7,7 +7,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 
-public class TesteLitura {
+public class TesteLeitura {
 
 	public static void main(String[] args) throws IOException {
 		InputStream fis = new FileInputStream("lorem.txt");
